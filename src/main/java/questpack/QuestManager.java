ee54f8728db87c40b91039a1571f2f3bcffe634b
localhostloco <daniel.susumu@hotmail.com>
2014-11-07 17:16:58 +0100

    Questpack created along with the classes we discussed at the meeting

---
+++ src/main/java/questpack/QuestManager.java
@@ -0,0 +1,18 @@
+package questpack;
+
+import org.bukkit.entity.Entity;
+
+public class QuestManager {
+
+	public boolean checkNpcClicled(Entity npc) {
+		return true;
+	}
+	
+	public void handleQuest() {
+		
+	}
+	
+	public boolean checkQuestFinished() {
+		return true;
+	}
+}
