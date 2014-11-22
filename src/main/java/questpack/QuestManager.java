bc878156b3abf555e1ef734ca598e49df06fd866
Bagdasaryan <173033@edu.p.lodz.pl>
2014-11-22 14:50:00 +0100

    adding reward for compliting the quest

--- src/main/java/questpack/QuestManager.java
+++ src/main/java/questpack/QuestManager.java
@@ -13,7 +13,7 @@ public class QuestManager {
        
         public static Quest[] generateQuests() {
                 Quest[] questList = new Quest[10];
-                questList[0] = new Quest("Lumberjack", "Pick up 10 wood and turn it in",10, 0, new Reward(Material.DIAMOND_AXE, 10),Material.WOOD);
+                questList[0] = new Quest("Lumberjack", "Pick up 10 wood and turn it in",10, 0, new Reward(Material.DIAMOND_AXE, 10),Material.DIRT);
                 questList[2] = new Quest("Bla bla bla", "Pick up 10 iron and turn it in",15, 1, new Reward(Material.DIAMOND_AXE, 10),Material.SEEDS);
                 questList[5] = new Quest("Abecad", "Pick up 10 wood and turn it in",20, 2, new Reward(Material.DIAMOND_AXE, 10),Material.SAND);
                 return questList;
@@ -29,12 +29,11 @@ public class QuestManager {
         	if (playerInventory.contains(questList[questNumber].getMaterial(), questList[questNumber].getAmountOfMaterial()))
         	{
         		
-        		mcMMOPlayer.getPlayer().sendMessage("You finish the Quest ! "+ questList[questNumber].getName());
+        		//mcMMOPlayer.getPlayer().sendMessage("You finish the Quest ! "+ questList[questNumber].getName());
         		questList[questNumber].setStateOfTheQuest(2);
         		return true;
         	}
         	else{
-        		
         		
         		mcMMOPlayer.getPlayer().sendMessage("You have to collect more items ! ");
         		questList[questNumber].setStateOfTheQuest(1);
@@ -50,28 +49,40 @@ public class QuestManager {
         	ItemStack itemStack = new ItemStack(questList[questNumber].getMaterial());
         	
         	//checkIsQuestFinished(mcMMOPlayer,questNumber);
-        	
-        	if((questList[questNumber].getQuestStatus()==2) && (checkIsQuestFinished(mcMMOPlayer,questNumber)))
-        	{
-        		mcMMOPlayer.getPlayer().sendMessage("ALREADY COMPLITED ! ");
-        	}
-        	else if ((questList[questNumber].getQuestStatus()==1) &&(!checkIsQuestFinished(mcMMOPlayer,questNumber)))
-        	{
-        		
-        		if (!playerInventory.contains(questList[questNumber].getMaterial(), questList[questNumber].getAmountOfMaterial()))
-        			mcMMOPlayer.getPlayer().sendMessage("In progress ! ");	
-        	}
-        	
-        	else if(questList[questNumber].getQuestStatus()==0)
-        	{
-        		mcMMOPlayer.getPlayer().sendMessage("Description: "+ questList[questNumber].getDescription());
-        	}
-
-        	
+        	 if (questList[questNumber].getQuestStatus() == 2) 
+        	 {
+        		 mcMMOPlayer.getPlayer().sendMessage("ALREADY COMPLETED ! ");
+        	 } 
+        	 else if (questList[questNumber].getQuestStatus() == 1)
+        	 {
+        		 if (checkIsQuestFinished(mcMMOPlayer, questNumber))
+        		 {
+        			 mcMMOPlayer.getPlayer().sendMessage("QUEST" + questList[questNumber].getName() + " COMPLETED");
+        			 for (int i=0;i<=questList[questNumber].getAmountOfMaterial();i++)
+        			 {
+        				 playerInventory.remove(questList[questNumber].getMaterial());
+        			 }
+        			 handleReward(mcMMOPlayer,questNumber);
+        		 }
+        	 }
+        	 else if (questList[questNumber].getQuestStatus() == 0)
+        	 {
+        		 mcMMOPlayer.getPlayer().sendMessage("Description: " + questList[questNumber].getDescription());
+        		 questList[questNumber].setStateOfTheQuest(1);
+        	 }
+        
         }
-        
-     
-        
-    
+        public static void handleReward(McMMOPlayer mcMMOPlayer, int questNumber) {
+        	  Player player = mcMMOPlayer.getPlayer();
+        	  PlayerInventory playerInventory = player.getInventory();
+        	  Quest[] questList = mcMMOPlayer.getQuestData();  
+        	  Reward reward = questList[questNumber].getReward();
+        	  ItemStack itemStack = new ItemStack(reward.getMaterial());
+
+        	  playerInventory.addItem(itemStack);
+        	  mcMMOPlayer.getPlayer().sendMessage("Quest reward: " + reward.getMaterial().toString());
+
+        	 }
+ 
         
 }
