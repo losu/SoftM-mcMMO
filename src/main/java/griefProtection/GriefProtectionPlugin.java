a1ed55377e799cebc855e3a0cd556e9d063212ba
Adama <adama_camara@hotmail.dk>
2014-11-30 13:40:41 +0100

    few changes to gief

--- src/main/java/griefProtection/GriefProtectionPlugin.java
+++ src/main/java/griefProtection/GriefProtectionPlugin.java
@@ -12,7 +12,7 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GriefProtectionPlugin extends JavaPlugin {
-	// insert code
+	
 	
 	public final Logger logger = Logger.getLogger("MineCraft");
 	public final ArrayList <Location> blocks = new ArrayList<Location>();
